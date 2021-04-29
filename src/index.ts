fc204b36b9906fa2ffbe2920c76314cb00d105be
Linus Willner <18148938+linuswillner@users.noreply.github.com>
2021-04-29 19:37:56 +0300

    Add Windows Event Logger driver  (#11)
    * Document constants
    
    * Add Windows Event Logger driver

--- src/index.ts
+++ src/index.ts
@@ -4,11 +4,12 @@ import 'reflect-metadata'
 // Setup logger
 import './common/logger'
 
-import dotenv from 'dotenv-safe'
 import express from 'express'
 import helmet from 'helmet'
 import v1Router from './api/v1/index'
 
+// Leaving this here, we might still need this
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 function terminate (): void {
   logger.error('BOOT: Encountered fatal error during boot process. Exiting...')
   process.exit(1)
@@ -16,10 +17,8 @@ function terminate (): void {
 
 // We can't reliably use TLA (Top-Level Await) yet, so we have to fallback to an async IIFE
 void (async () => {
-  // Load configuration
-  logger.info('BOOT: Loading configuration.')
-  dotenv.config()
-  logger.info('BOOT: Configuration loaded.')
+  logger.info('BOOT: Starting up.')
+  logger.info(`BOOT: Running in ${process.env.NODE_ENV === 'development' ? 'development' : 'production'} mode.`)
 
   const app = express()
 
@@ -32,17 +31,8 @@ void (async () => {
   // Apply routers
   app.use('/api/v1', v1Router)
 
-  const host = process.env.SERVER_HOST
-  const port = process.env.SERVER_PORT
-
-  if (!host || !port || isNaN(parseInt(port))) {
-    logger.error(`BOOT: SERVER_HOST and/or SERVER_PORT not defined correctly; expected string and number, got SERVER_HOST=${host} SERVER_PORT=${port}`)
-    terminate()
-    return // This is just here to satisfy TS since it doesn't know that this function just terminates the program
-  }
-
-  app.listen(parseInt(port), host, () => {
-    logger.info(`BOOT: REST server listening on http://${host}:${port}.`)
+  app.listen(63636, 'localhost', () => {
+    logger.info('BOOT: REST server listening on http://localhost:63636.')
     logger.info('BOOT: Startup complete.')
   })
 })()
