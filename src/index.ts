74126bc7ee406830ff6656e7f14b5a01035a1c36
Linus Willner <hello@linuswillner.me>
2021-04-29 19:21:14 +0300

    Remove redundant top-level async IIFE

--- src/index.ts
+++ src/index.ts
@@ -14,35 +14,21 @@ function terminate (): void {
   process.exit(1)
 }
 
-// We can't reliably use TLA (Top-Level Await) yet, so we have to fallback to an async IIFE
-void (async () => {
-  // Load configuration
-  logger.info('BOOT: Loading configuration.')
-  dotenv.config()
-  logger.info('BOOT: Configuration loaded.')
+logger.info('BOOT: Starting up.')
+logger.info(`BOOT: Running in ${process.env.NODE_ENV === 'development' ? 'development' : 'production'} mode.`)
 
-  const app = express()
+const app = express()
 
-  // Parse bodies as JSON
-  app.use(express.json())
+// Parse bodies as JSON
+app.use(express.json())
 
-  // Load Helmet
-  app.use(helmet())
+// Load Helmet
+app.use(helmet())
 
-  // Apply routers
-  app.use('/api/v1', v1Router)
+// Apply routers
+app.use('/api/v1', v1Router)
 
-  const host = process.env.SERVER_HOST
-  const port = process.env.SERVER_PORT
-
-  if (!host || !port || isNaN(parseInt(port))) {
-    logger.error(`BOOT: SERVER_HOST and/or SERVER_PORT not defined correctly; expected string and number, got SERVER_HOST=${host} SERVER_PORT=${port}`)
-    terminate()
-    return // This is just here to satisfy TS since it doesn't know that this function just terminates the program
-  }
-
-  app.listen(parseInt(port), host, () => {
-    logger.info(`BOOT: REST server listening on http://${host}:${port}.`)
-    logger.info('BOOT: Startup complete.')
-  })
-})()
+app.listen(63636, 'localhost', () => {
+  logger.info('BOOT: REST server listening on http://localhost:63636.')
+  logger.info('BOOT: Startup complete.')
+})
