38a334489da1495933c8f74811e3480d63130574
Linus Willner <hello@linuswillner.me>
2021-04-30 20:26:31 +0300

    Fix arg handling in uninstall/deregister

--- src/cli/handlers/deregister.ts
+++ src/cli/handlers/deregister.ts
@@ -3,14 +3,12 @@ import deregister from '../../api/deregister'
 import { getCredential } from '../../common/credentialStore'
 import { parseCLIArgs } from '../utils'
 
-export default async (args: yargs.Argv, options?: { failIfAlreadyDone?: boolean, needsArgs?: boolean }): Promise<void> => {
+export default async (args: yargs.Argv, options?: { failIfAlreadyDone?: boolean, needsArgs?: boolean }): Promise<void> => {´
+  if (options?.needsArgs) parseCLIArgs(args)
+
   if (options?.failIfAlreadyDone && (await getCredential('host') === null || await getCredential('token') === null)) {
     logger.warn('Daemon is already de-registered.')
     process.exit(1)
-  }
-
-  if (options?.needsArgs) {
-    const { argv } = parseCLIArgs(args)
   }
 
   await deregister()
--- src/cli/handlers/uninstall.ts
+++ src/cli/handlers/uninstall.ts
@@ -4,13 +4,11 @@ import { getCredential } from '../../common/credentialStore'
 import { parseCLIArgs } from '../utils'
 
 export default async (args: yargs.Argv, options?: { failIfAlreadyDone?: boolean, needsArgs?: boolean }): Promise<void> => {
+  if (options?.needsArgs) parseCLIArgs(args)
+
   if (options?.failIfAlreadyDone && (await getCredential('host') === null || await getCredential('token') === null)) {
     logger.warn('Daemon is already de-registered.')
     process.exit(1)
-  }
-
-  if (options?.needsArgs) {
-    const { argv } = parseCLIArgs(args)
   }
 
   await deregister()
